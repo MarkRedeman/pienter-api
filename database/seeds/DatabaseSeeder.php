57eb6d6473c201e497119cad28ce33db63487b07
Mark Redeman <markredeman@gmail.com>
2018-08-31 16:34:57 +0200

    Update soda price

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         $products = [
             ['id' => 1, 'name' => 'Beer', 'price' => 50, 'category' => 'Bier'],
             ['id' => 2, 'name' => 'Wine', 'price' => 50, 'category' => 'Bier'],
-            ['id' => 3, 'name' => 'Soda', 'price' => 50, 'category' => 'Fris'],
+            ['id' => 3, 'name' => 'Soda', 'price' => 30, 'category' => 'Fris'],
             ['id' => 4, 'name' => 'Straw', 'price' => 0, 'category' => 'Eten'],
         ];
 
